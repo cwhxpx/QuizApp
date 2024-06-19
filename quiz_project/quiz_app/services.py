95495cde51011b61276da8f8d0a0b54069065b2c
sunshuwei <cwh.xpx@qq.com>
2024-06-19 10:44:47 +1000

    add function of download test questions.

--- quiz_project/quiz_app/services.py
+++ quiz_project/quiz_app/services.py
@@ -1,13 +1,43 @@
 from openai import OpenAI
 from . import config
+import sqlite3
 
 # new openai api client
 openai_client = OpenAI(
     api_key=config.OPENAI_API_KEY
 )
 
+# new db
+def initialize_database():
+    conn = sqlite3.connect('questions.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            key TEXT UNIQUE,
+            value TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def print_all_questions():
+    initialize_database()
+    conn = sqlite3.connect('questions.db')
+    cursor = conn.cursor()
+    # Retrieve all rows from the database
+    cursor.execute("SELECT * FROM questions")
+    rows = cursor.fetchall()
+    return rows
+
+
 # generating question
 def generate_questions(text):
+    # db
+    initialize_database()
+    # connect db
+    conn = sqlite3.connect('questions.db')
+    cursor = conn.cursor()
     # Define your prompt for generating questions
     prompt = (
         f"Create a practice test with multiple choice questions on the following text:\n{text}\n\n"
@@ -25,4 +55,23 @@ def generate_questions(text):
 
     # Extract the generated questions from the API response
     questions = response.choices[0].message.content
+
+    # Generate a unique key for the question
+    base_key = ' '.join(text.split()[:2])
+    key = base_key
+    index = 1
+    while key_exists(cursor, key):
+        key = f"{base_key} {index}"
+        index += 1
+
+    # Insert the questions into the database
+    value = questions
+    cursor.execute("INSERT INTO questions (key, value) VALUES (?, ?)",(key, value))
+    conn.commit()
+
     return questions
+
+def key_exists(cursor, key):
+    cursor.execute("SELECT COUNT(*) FROM questions WHERE key = ?", (key,))
+    count = cursor.fetchone()[0]
+    return count > 0
--- quiz_project/quiz_project/urls.py
+++ quiz_project/quiz_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz_app.urls')),
+    path('history/', include('quiz_app.urls')),
 ]
